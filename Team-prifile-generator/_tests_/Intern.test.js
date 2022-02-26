ee9194edc39f3f5457f2e3231dd268172a076713
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-02-26 12:59:29 -0700

    Delete Team-prifile-generator directory

--- Team-prifile-generator/_tests_/Intern.test.js
+++
@@ -1,6 +0,0 @@
-const Intern = require('../lib/Intern');
-
-test("intern for school as object", () => {
-    const intern = new Intern("Sam", 'C4500', 'sam@gmail.com', 'U of AZ')
-    expect(intern.getSchool()).toBe('U of AZ')
-})
